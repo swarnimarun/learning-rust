f5e482671aacb1f474f90d3fdcb5995a32b2a004
Swarnim Arun <swarnimarun11@gmail.com>
2019-03-16 07:41:51 +0530

    rscat app v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 
-mod test; // to use the file test.rs
+// let's create a proper cli tool
 
-fn main() -> Result<(), std::io::Error> {
-    test::do_something();
-    Ok(())
+// let's start with a proper cli parser first
+use clap::{Arg, App};
+
+fn main() {
+    let matches = App::new("rscat")
+        .version("0.1.0")
+        .author("Swarnim Arun <swarnimarun11@gmail.com>")
+        .about("cat command clone in rust")
+        .arg(Arg::with_name("file_path")
+                 .required(true)
+                 .takes_value(true)
+                 .index(1)
+                 .help("path to file that is to be read"))
+        .get_matches();
+    
+    let path = matches.value_of("file_path").unwrap();
+    println!("{}", std::fs::read_to_string(path).unwrap());
 }
